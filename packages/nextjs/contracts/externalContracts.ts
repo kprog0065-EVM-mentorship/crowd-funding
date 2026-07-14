0a7a9358faabb69fc9d5748784dce54ffbd464db
Kevon Jaggassar <jaggassarkevon@gmail.com>
2026-07-13 21:45:54 -0400

    chore: edited externalContracts to accept abi info from deployments

--- packages/nextjs/contracts/externalContracts.ts
+++ packages/nextjs/contracts/externalContracts.ts
@@ -1,16 +1,13 @@
+import { Abi_CrowdFund } from "../../hardhat/generated/abis/CrowdFund";
 import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 
-/**
- * @example
- * const externalContracts = {
- *   1: {
- *     DAI: {
- *       address: "0x...",
- *       abi: [...],
- *     },
- *   },
- * } as const;
- */
-const externalContracts = {} as const;
+const externalContracts = {
+  11155111: {
+    CrowdFund: {
+      address: "0xfa0782f654ddc4fc347f43e374a39327d05824b6",
+      abi: Abi_CrowdFund,
+    },
+  },
+} as const;
 
 export default externalContracts satisfies GenericContractsDeclaration;
